cc4d25d2b92f050e0e2b6e6af63bc22011fbaaa1
MarcoNicolodi <marcocontatopro@gmail.com>
2018-12-26 22:45:33 -0200

    Add material ui base globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import MenuIcon from '@material-ui/icons/Android';
 
 const App = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-    <Typography variant="h1">Hello World!</Typography>
-    {/* <Icon color="action">
-        add_circle
-    </Icon> */}
-    <MenuIcon />
-    <Button variant="contained" color="primary">Oi</Button>
-  </div>);
+  <CssBaseline>
+    <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+      <Typography variant="h1">Hello World!</Typography>
+      <Button variant="contained" color="primary">
+      Hello World
+      </Button>
+      <MenuIcon />
+      <Button variant="contained" color="primary">Oi</Button>
+    </div>
+  </CssBaseline>
+);
 
 export default App;
